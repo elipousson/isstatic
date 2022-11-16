2fef000f1e2c1a53bd638af93c55c1330f5f0167
Eli Pousson <eli.pousson@gmail.com>
2022-11-16 13:27:22 -0500

    feat: add is_all functions

---
+++ R/is_all.R
@@ -0,0 +1,24 @@
+#' Do all items in a list or vector return TRUE from a predicate function?
+#'
+#' @param x A list or vector passed to [vapply()].
+#' @param FUN Function passed to FUN parameter of [vapply()].
+#' @noRd
+is_all <- function(x, FUN) {
+  all(vapply(x, FUN, FUN.VALUE = TRUE))
+}
+
+#' Are all items in a list or vector NULL values?
+#'
+#' @param x A list or vector to check.
+#' @noRd
+is_all_null <- function(x) {
+  is_all(x, is.null)
+}
+
+#' Are all items in a list or vector NA values?
+#'
+#' @param x A list or vector to check.
+#' @noRd
+is_all_na <- function(x) {
+  is_all(x, is.na)
+}
--- inst/staticexports/is_any.R
+++ inst/staticexports/is_any.R
@@ -1,4 +1,4 @@
-#' Is any item in a list or vector return TRUE from a predicate function?
+#' Do any items in a list or vector return TRUE from a predicate function?
 #'
 #' @param x A list or vector passed to [vapply()].
 #' @param FUN Function passed to FUN parameter of [vapply()].
@@ -7,7 +7,7 @@ is_any <- function(x, FUN) {
   any(vapply(x, FUN, FUN.VALUE = TRUE))
 }
 
-#' Is any item in a list or vector NULL?
+#' Is any item in a list or vector a NULL value?
 #'
 #' @param x A list or vector to check.
 #' @noRd
@@ -15,7 +15,7 @@ is_any_null <- function(x) {
   is_any(x, is.null)
 }
 
-#' Check if any item in a list or vector is NA
+#' Is any item in a list or vector a NA value?
 #'
 #' @param x A list or vector to check.
 #' @noRd
