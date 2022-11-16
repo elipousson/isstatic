d66fd55aee1a1c8155d70ee62871c915c0ed1dd6
Eli Pousson <eli.pousson@gmail.com>
2022-11-16 13:55:55 -0500

    refactor(has_filetype): make NULL string return FALSE

--- R/has_filetype.R
+++ R/has_filetype.R
@@ -3,12 +3,20 @@
 
 #' Does string contain a filetype or the specified filetype?
 #'
-#' @param string String to be tested with or without filetype.
+#' Check if string contains any filetype or the provided filetype. If string is
+#' `NULL`, returns `FALSE`.
+#'
+#' @param string String to be tested with or without filetype. Defaults to
+#'   `NULL`.
 #' @param filetype File type to test against. Optional.
 #' @param ignore.case If `FALSE`, the pattern matching is case sensitive. If
 #'   `TRUE`, case is ignored.
 #' @export
-has_filetype <- function(string, filetype = NULL, ignore.case = FALSE) {
+has_filetype <- function(string = NULL, filetype = NULL, ignore.case = FALSE) {
+  if (is.null(string)) {
+    return(FALSE)
+  }
+
   if (is.null(filetype)) {
     filetype <- "[a-zA-Z0-9]+"
   }
