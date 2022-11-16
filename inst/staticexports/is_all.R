a248f8f5c24113a553f72e7d30e0f53c5431e4c9
Eli Pousson <eli.pousson@gmail.com>
2022-11-16 13:33:25 -0500

    feat(is_all): relocate + export is_all functions

--- inst/staticexports/is_all.R
+++ inst/staticexports/is_all.R
@@ -1,11 +1,8 @@
-# `R/is_all.R` is imported from `inst/staticexports/is_all.R`. 
-# Please edit that file instead.
-
 #' Do all items in a list or vector return TRUE from a predicate function?
 #'
 #' @param x A list or vector passed to [vapply()].
 #' @param FUN Function passed to FUN parameter of [vapply()].
-#' @export
+#' @noRd
 is_all <- function(x, FUN) {
   all(vapply(x, FUN, FUN.VALUE = TRUE))
 }
@@ -13,7 +10,7 @@ is_all <- function(x, FUN) {
 #' Are all items in a list or vector NULL values?
 #'
 #' @param x A list or vector to check.
-#' @export
+#' @noRd
 is_all_null <- function(x) {
   is_all(x, is.null)
 }
@@ -21,7 +18,7 @@ is_all_null <- function(x) {
 #' Are all items in a list or vector NA values?
 #'
 #' @param x A list or vector to check.
-#' @export
+#' @noRd
 is_all_na <- function(x) {
   is_all(x, is.na)
 }
