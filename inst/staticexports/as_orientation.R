d1eef75d676995a83b1c5395aef98feb3af80282
Eli Pousson <eli.pousson@gmail.com>
2022-12-29 23:47:38 -0800

    fix(as_orientation): correct internal calls to as_orientation
    - fix(as_orientation): correct internal calls to as_orientation
    - test(as_orientation): add tests
    - docs(as_orientation): add missing returns description

--- inst/staticexports/as_orientation.R
+++ inst/staticexports/as_orientation.R
@@ -1,25 +1,31 @@
 #' What is the orientation of a numeric aspect ratio?
 #'
 #' @param x A numeric vector with an aspect ratio or a data.frame with width and
-#'   height column (names matching the cols parameter).
+#'   height column (using width and height values from columns matching the cols
+#'   parameter).
 #' @param tolerance Positive numeric value above or below 1 used to determine if
 #'   an aspect ratio is square, landscape, or portrait.
 #' @param cols Name of width and height column if x is a data.frame object.
+#' @returns A character vector of orientations of the same length as x or, if x
+#'   is a data.frame, the same length as the number of rows in x.
 #' @noRd
 as_orientation <- function(x, tolerance = 0.1, cols = c("width", "height")) {
   tolerance <- abs(tolerance)
 
   if (is.data.frame(x)) {
+    check_name(x, cols)
     return(
-      has_orientation(
+      as_orientation(
         as.numeric(x[, cols[1]]) / as.numeric(x[, cols[2]]),
         tolerance
       )
     )
   }
 
+  check_numeric(x)
+
   if (length(x) > 1) {
-    return(map_chr(x, has_orientation, tolerance))
+    return(map_chr(x, as_orientation, tolerance))
   }
 
   if (x > (1 + tolerance)) {
@@ -31,16 +37,4 @@ as_orientation <- function(x, tolerance = 0.1, cols = c("width", "height")) {
   }
 
   "square"
-}
-
-
-# from staticimports
-# https://github.com/wch/staticimports/blob/main/inst/staticexports/purrr.R
-#' @noRd
-map_chr <- function(.x, .f, ...) {
-  if (is.character(.f)) {
-    vapply(.x, `[[`, .f, ..., FUN.VALUE = NA_character_)
-  } else {
-    vapply(.x, .f, ..., FUN.VALUE = NA_character_)
-  }
 }
---
+++ tests/testthat/test-as_orientation.R
@@ -0,0 +1,12 @@
+test_that("as_orientation works", {
+  expect_identical(as_orientation(6/4), "landscape")
+  expect_identical(as_orientation(4/6), "portrait")
+  expect_identical(as_orientation(4/4), "square")
+  expect_identical(as_orientation(data.frame(
+    "width" = c(6, 4, 4),
+    "height" = c(4, 6, 4)
+  )), c("landscape", "portrait", "square"))
+
+  expect_identical(as_orientation(1.09 / 1), "square")
+  expect_identical(as_orientation(1.08 / 1, tolerance = 0.07), "landscape")
+})
