97e4473bf0f33927f04121881bfcfb54f161cc98
Eli Pousson <eli.pousson@gmail.com>
2023-01-17 22:34:36 -0500

    refactor: reorganize sf related functions

---
+++ R/as_crs.R
@@ -0,0 +1,66 @@
+# `R/as_crs.R` is imported from `inst/staticexports/as_crs.R`. 
+# Please edit that file instead.
+
+
+#' Coerce a sf, sfc, or bbox object to a coordinate reference system
+#'
+#' This function should be updated to support stars and terra objects.
+#'
+#' @param x A sf, sfc, or bbox object to coerce into a CRS.
+#' @param input If `TRUE` (default), return only the "input" component of the
+#'   crs object. If `FALSE`, return the full crs object.
+#' @export
+as_crs <- function(x, input = TRUE) {
+  #  Previously: Character strings starting with EPSG: or ESRI: are returned as
+  #  is. Numeric vectors are returned as is without checking and may or may not
+  #  have been converted to a crs.
+
+  # if ((is.character(x) && grepl("^(EPSG|ESRI):", x)) | is.numeric(x)) {
+  #   return(x)
+  # }
+
+  if (is_sf(x)) {
+    x <- as_sfc(x)
+  }
+
+  check_if(
+    is_sfc(x) | is_bbox(x),
+    "`as_crs()` requires a <sf>, <sfc>, or <bbox> object."
+  )
+
+  crs <- attributes(x)[["crs"]]
+
+  if (is.na(crs)) {
+    return(NA)
+  }
+
+  if (isTRUE(input)) {
+    return(crs[["input"]])
+  }
+
+  crs
+}
+
+#' - [is_lonlat_crs()]: Is this sf, sfc, or bbox class object with a geographic coordinate reference
+#' system ("EPSG:4326" or "EPSG:4269")?
+#'
+#' @name is_lonlat_crs
+#' @rdname as_crs
+#' @export
+is_lonlat_crs <- function(x, crs = c("EPSG:4326", "EPSG:4269")) {
+  if (!is_sf_ext(x)) {
+    return(FALSE)
+  }
+
+  as_crs(x) %in% crs
+}
+
+#' - [has_same_crs()]: Do two sf, sfc, or bbox objects use the same coordinate
+#' reference system?
+#'
+#' @param x,y sf, sfc, or bbox objects to be compared.
+#' @param ... Additional parameters passed to [identical()] by [has_same_crs()].
+#' @export
+has_same_crs <- function(x, y, ...) {
+  identical(as_crs(x), as_crs(y), ...)
+}
--- inst/staticexports/is_sf.R
+++ inst/staticexports/is_sf.R
@@ -1,6 +1,7 @@
 #' Is this a sf class object?
 #'
-#' @param x Object to be tested.
+#' @param x An object to be tested with [inherits()]
+#' @seealso [isstatic::as_crs()]
 #' @noRd
 is_sf <- function(x) {
   inherits(x, "sf")
@@ -9,7 +10,8 @@ is_sf <- function(x) {
 
 #' Is this a sfc class object?
 #'
-#' @param x Object to be tested.
+#' @name is_sfc
+#' @rdname is_sf
 #' @noRd
 is_sfc <- function(x) {
   inherits(x, "sfc")
@@ -18,7 +20,8 @@ is_sfc <- function(x) {
 
 #' Is this a sfg class object?
 #'
-#' @param x Object to be tested.
+#' @name is_sfg
+#' @rdname is_sf
 #' @noRd
 is_sfg <- function(x) {
   inherits(x, "sfg")
@@ -27,7 +30,8 @@ is_sfg <- function(x) {
 
 #' Is this a bbox class object?
 #'
-#' @param x Object to be tested.
+#' @name is_bbox
+#' @rdname is_sf
 #' @noRd
 is_bbox <- function(x) {
   inherits(x, "bbox")
@@ -35,7 +39,8 @@ is_bbox <- function(x) {
 
 #' Is this a sf, sfc, or bbox class object?
 #'
-#' @param x Object to be tested.
+#' @name is_sf_ext
+#' @rdname is_sf
 #' @param ext If `TRUE`, return `TRUE` is x is a sf, sfc, or bbox object. If
 #'   `FALSE`, only check if x is an sf object. If ext is a character object, it
 #'   is passed to the what parameter of [inherits()] with sf.
@@ -55,8 +60,9 @@ is_sf_ext <- function(x, ext = TRUE) {
 
 #' Is this a RasterLayer class object?
 #'
-#' @param x Object to be tested.
-#' @export
+#' @name is_raster
+#' @rdname is_sf
+#' @noRd
 is_raster <- function(x) {
   inherits(x, "RasterLayer")
 }
@@ -64,69 +70,9 @@ is_raster <- function(x) {
 
 #' Is this a Spatial class (sp) object?
 #'
-#' @param x Object to be tested.
-#' @export
+#' @name is_sp
+#' @rdname is_sf
+#' @noRd
 is_sp <- function(x) {
   inherits(x, "Spatial")
-}
-
-
-#' Is this sf, sfc, or bbox class object in a geographic coordinate system?
-#'
-#' This function currently only checks for "EPSG:4326" and "EPSG:4269" CRS.
-#'
-#' @param x Object to be tested.
-#' @noRd
-is_lonlat <- function(x, crs = c("EPSG:4326", "EPSG:4269")) {
-  if (!inherits(x, c("sf", "sfc", "bbox"))) {
-    return(FALSE)
-  }
-
-  is_any_in(as_crs(x), crs)
-}
-
-#' Do two sf, sfc, or bbox objects use the same coordinate reference system?
-#'
-#' @param x,y sf, sfc, or bbox objects to be compared.
-#' @param ... Additional parameters passed to [identical()]
-#' @noRd
-has_same_crs <- function(x, y, ...) {
-  identical(as_crs(x), as_crs(y), ...)
-}
-
-#' Coerce a sf, sfc, or bbox object to a coordinate reference system
-#'
-#' This function should be updated to support stars objects.
-#'
-#' @param x A sf, sfc, or bbox object to coerce into a CRS. Character strings
-#'   starting with EPSG: or ESRI: are returned as is. Numeric vectors are
-#'   returned as is without checking.
-#' @param input If `TRUE` (default), return only the "input" component of the
-#'   crs object. If `FALSE`, return the full crs object.
-#' @noRd
-as_crs <- function(x, input = TRUE) {
-  if ((is.character(x) && grepl("^(EPSG|ESRI):", x)) | is.numeric(x)) {
-    return(x)
-  }
-
-  if (is_sf(x)) {
-    x <- as_sfc(x)
-  }
-
-  check_if(
-    is_sfc(x) | is_bbox(x),
-    "`as_crs()` requires a <sf>, <sfc>, or <bbox> object."
-  )
-
-  crs <- attributes(x)[["crs"]]
-
-  if (is.na(crs)) {
-    return(NA)
-  }
-
-  if (isTRUE(input)) {
-    return(crs[["input"]])
-  }
-
-  crs
 }
