2d7c24711ef5a73bbc07928d8babf268e394a965
Eli Pousson <eli.pousson@gmail.com>
2023-03-20 00:12:54 -0400

    refactor: drop static_check_character from file_path

--- R/path.R
+++ R/path.R
@@ -6,25 +6,32 @@
 #' A replacement for `file.path()`
 #'
 #' @param ... Additional strings to pass before path and filename.
-#' @param filename File name. Optional if path is supplied.
 #' @param path Path name. Optional if filename is supplied.
+#' @param filename File name. Optional if path is supplied.
 #' @inheritParams base::file.path
 #' @param allow_null If `TRUE`, return `NULL` if filename and path are `NULL`
 #'   and no additional strings are provided to .... If `FALSE`, stop if filename
 #'   and path are `NULL` and no additional strings are provided to ...
 #' @export
 file_path <- function(...,
-                      filename = NULL,
                       path = NULL,
+                      filename = NULL,
                       fsep = .Platform$file.sep,
-                      allow_null = FALSE) {
+                      allow_null = FALSE,
+                      call = parent.frame()) {
   path <- str_c(..., path, filename, sep = fsep)
+  path_has_null <- any(identical(path, character(0)))
 
-  if (identical(path, character(0)) && isTRUE(allow_null)) {
+  if (isTRUE(path_has_null) && isTRUE(allow_null)) {
     return(NULL)
   }
 
-  static_check_character(path)
+  if (isFALSE(all(is.character(path)) && isFALSE(path_has_null))) {
+    stop(
+      "! `file_path()` must return a character vector when `allow_null = FALSE`.",
+      call. = call
+    )
+  }
 
   path
 }
