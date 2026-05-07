2bcbe9035973ab8acb2570c37e22574b741dd566
Eli Pousson <eli.pousson@gmail.com>
2026-05-07 01:31:53 -0400

    Style tests

--- tests/testthat/test-as_cardinal_bearing.R
+++ tests/testthat/test-as_cardinal_bearing.R
@@ -1,6 +1,7 @@
 test_that("as_cardinal_bearing works", {
   expect_identical(
-    as_cardinal_bearing(c(5, 95)), c("N" = 0, "E" = 90)
+    as_cardinal_bearing(c(5, 95)),
+    c("N" = 0, "E" = 90)
   )
   expect_identical(
     as_cardinal_bearing(data.frame("bearing" = c(5, 95))),
