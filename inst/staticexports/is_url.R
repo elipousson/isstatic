0d5a8af342a8d3574265dab4315e1face662869e
Eli Pousson <eli.pousson@gmail.com>
2026-05-08 23:48:15 -0400

    Fix formatting of functions

--- inst/staticexports/is_url.R
+++ inst/staticexports/is_url.R
@@ -9,7 +9,7 @@ is_url <- function(x) {
   )
 }
 
-#' - [is_esri_url()]: Is an object an ArcGIS MapServer or FeatureServer URL?
+#' - `is_esri_url()`: Is an object an ArcGIS MapServer or FeatureServer URL?
 #'
 #' @name is_esri_url
 #' @rdname is_url
@@ -18,7 +18,7 @@ is_esri_url <- function(x) {
   is_url(x) & grepl("/MapServer|/FeatureServer", x)
 }
 
-#' - [is_gsheet_url()]: Is an object a Google Sheets URL?
+#' - `is_gsheet_url()`: Is an object a Google Sheets URL?
 #'
 #' @name is_gsheet_url
 #' @rdname is_url
@@ -27,7 +27,7 @@ is_gsheet_url <- function(x) {
   grepl("^https://docs.google.com/spreadsheets/", x)
 }
 
-#' - [is_gist_url()]: Is an object a URL for a GitHub Gist?
+#' - `is_gist_url()`: Is an object a URL for a GitHub Gist?
 #'
 #' @name is_gist_url
 #' @rdname is_url
@@ -36,7 +36,7 @@ is_gist_url <- function(x) {
   grepl("^https://gist.github.com/", x)
 }
 
-#' - [is_gmap_url()]: Is an object a Google Maps URL?
+#' - `is_gmap_url()`: Is an object a Google Maps URL?
 #'
 #' @name is_gmap_url
 #' @rdname is_url
